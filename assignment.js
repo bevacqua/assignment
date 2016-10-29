95132131b4d2b3cf6cc271ea133c7cbf34e1d672
Glenn McGuire <glennmcguire9@gmail.com>
2016-10-29 14:58:20 +1100

    Reformat code

--- assignment.js
+++ assignment.js
@@ -1,26 +1,26 @@
 'use strict';
 
-function assignment (result) {
-  var stack = Array.prototype.slice.call(arguments, 1);
-  var item;
-  var key;
-  while (stack.length) {
-    item = stack.shift();
-    for (key in item) {
-      if (item.hasOwnProperty(key)) {
-        if (typeof result[key] === 'object' && result[key] && Object.prototype.toString.call(result[key]) !== '[object Array]') {
-          if (typeof item[key] === 'object' && item[key] !== null) {
-            result[key] = assignment(result[key], item[key]);
-          } else {
-            result[key] = item[key];
-          }
-        } else {
-          result[key] = item[key];
+function assignment(result) {
+    var stack = Array.prototype.slice.call(arguments, 1);
+    var item;
+    var key;
+    while (stack.length) {
+        item = stack.shift();
+        for (key in item) {
+            if (item.hasOwnProperty(key)) {
+                if (typeof result[key] === 'object' && result[key] && Object.prototype.toString.call(result[key]) !== '[object Array]') {
+                    if (typeof item[key] === 'object' && item[key] !== null) {
+                        result[key] = assignment(result[key], item[key]);
+                    } else {
+                        result[key] = item[key];
+                    }
+                } else {
+                    result[key] = item[key];
+                }
+            }
         }
-      }
     }
-  }
-  return result;
+    return result;
 }
 
 module.exports = assignment;
